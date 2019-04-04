550ef0e1294f0886d3482ff5d5f60bf56d3f0a62
Lucasli2018 <1906859953@qq.com>
2019-04-04 14:14:10 +0800

    review jdbc and mybatis

--- demo/src/main/java/com/lucas/javase/mybatis/demo/App.java
+++ demo/src/main/java/com/lucas/javase/mybatis/demo/App.java
@@ -12,8 +12,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.err.println("写一个错误error");
+        System.out.println("日志 org.apache.commons.logging.Log");
         Log log=LogFactory.getLog(App.class);
         
-        log.info("日志--->org.apache.commons.logging.Log");
+        log.info("日志 org.apache.commons.logging.Log");
+        log.error("写一个错误error");
     }
 }
